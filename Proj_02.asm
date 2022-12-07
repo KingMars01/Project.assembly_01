eec835eecdc74f93f36b091de5caf4a0c836601d
KingMars01 <111364309+KingMars01@users.noreply.github.com>
2022-12-07 19:49:41 -0300

    Add files via upload

--- Proj_02.asm
+++ Proj_02.asm
@@ -31,6 +31,14 @@ TITLE SUDOKU
         MOV DL,179
         INT 21h
     ENDM
+
+    DIVISAO1 MACRO ; PULA LINHA + IMPRESSÃO
+        MOV AH,09
+        LEA DX,DIVISAO
+        INT 21h 
+                
+         
+    ENDM 
     
     SPACE MACRO
         MOV DL,32
@@ -68,7 +76,7 @@ LINHA EQU 9
 
 COLUNA EQU 9
   
-    FACIL   DB 0,6,0, 5,0,1, 8,0,0
+FACIL       DB 0,6,0, 5,0,1, 8,0,0
             DB 4,7,3, 0,0,2, 0,0,5
             DB 5,0,1, 0,0,0, 0,2,4
             
@@ -78,9 +86,9 @@ COLUNA EQU 9
             
             DB 0,0,5, 2,0,7, 4,8,0
             DB 9,4,6, 1,0,0, 7,5,0
-            DB 0,0,8, 9,0,0, 0,1,0
+            DB 0,0,8, 9,0,0, 0,1,0,'$'
 
-    FACILR  DB 2,6,9, 5,4,1, 8,7,3
+FACILR      DB 2,6,9, 5,4,1, 8,7,3
             DB 4,7,3, 8,9,2, 1,6,5
             DB 5,8,1, 3,7,6, 9,2,4
             
@@ -90,9 +98,33 @@ COLUNA EQU 9
             
             DB 1,3,5, 2,6,7, 4,8,9
             DB 9,4,6, 1,8,3, 7,5,2
-            DB 7,2,8, 9,5,4, 3,1,6      
+            DB 7,2,8, 9,5,4, 3,1,6,'$'
 
-    DIFICIL DB 0,0,0, 0,7,0, 9,1,3
+MEDIO       DB 1,0,0, 0,0,3, 2,0,0
+            DB 3,0,0, 0,0,0, 0,5,1
+            DB 0,0,0, 0,1,0, 7,0,0
+
+            DB 0,1,0, 6,0,2, 0,9,4
+            DB 0,9,0, 8,0,0, 0,0,7
+            DB 0,7,2, 0,0,0, 1,0,3
+
+            DB 0,3,0, 0,0,4, 9,0,0
+            DB 0,8,0, 0,0,0, 4,0,0
+            DB 4,0,9, 0,5,0, 0,8,2,'$'
+
+MEDIOR      DB 1,5,8, 7,6,3, 2,4,9
+            DB 3,4,7, 9,2,8, 6,5,1
+            DB 9,2,6, 4,1,5, 7,3,8
+
+            DB 5,1,3, 6,7,2, 8,9,4
+            DB 6,9,4, 8,3,1, 5,2,7
+            DB 8,7,2, 5,4,9, 1,6,3
+
+            DB 7,3,5, 2,8,4, 9,1,6
+            DB 2,8,1, 3,9,6, 4,7,5
+            DB 4,6,9, 1,5,7, 3,8,2,'$'
+
+DIFICIL     DB 0,0,0, 0,7,0, 9,1,3
             DB 0,0,2, 9,0,0, 6,0,0
             DB 1,0,0, 6,0,0, 0,0,0
 
@@ -102,9 +134,9 @@ COLUNA EQU 9
             
             DB 0,2,6, 0,0,0, 0,4,0
             DB 8,0,0, 3,0,4, 0,0,0
-            DB 0,7,0, 0,0,0, 0,9,1
+            DB 0,7,0, 0,0,0, 0,9,1,'$'
 
-  DIFICILR  DB 5,6,8, 4,7,2, 9,1,3
+DIFICILR    DB 5,6,8, 4,7,2, 9,1,3
             DB 3,4,2, 9,5,1, 6,8,7
             DB 1,9,7, 6,3,8, 2,5,4
             
@@ -114,14 +146,20 @@ COLUNA EQU 9
             
             DB 9,2,6, 8,1,7, 3,4,5
             DB 8,5,1, 3,9,4, 7,2,6
-            DB 4,7,3, 2,6,5, 8,9,1
+            DB 4,7,3, 2,6,5, 8,9,1,'$'
 
-        MSG1 db 10,'            SUDOKU GAME', '$'
-        LINHA1 DB ,'----------------------------------------'
+;MSG1    db 10,'            SUDOKU GAME','$'
+;LINHA2  DB 10,13,204,2 DUP(11 DUP (205),206),11 DUP (205),185,'$'
+;MSG2    DB 10,'        SELECIONE A DIFICULDADE:','$'
+;MSG3    DB 10,'        FACIL(1) MEDIO(2) DIFICIL(3)','$'
+DIVISAO db  179,'-------',179,'-------',179,'-------',179,'$'
+OLA DB 'OLA ','$'
+
+        
  
 
 .CODE 
-    MAIN PROC 
+MAIN PROC 
         MOV AX,03
         INT 10H
 
@@ -134,52 +172,52 @@ COLUNA EQU 9
         FINALIZA_PROG
 
    MAIN ENDP 
-   
-
-   IMPRIME_M PROC NEAR 
+IMPRIME_M PROC 
         
         PUSHMACRO
         
         MOV AH,02
         MOV CX,LINHA
-VOLTA:
+    VOLTA:
         MOV DI,3
-        XOR SI,SI
-
+        XOR SI,SI  
     
-    P_LINHA
-    PRINT LINHA1
-    BARRA2
-QUADRANTE:
+    BARRA1
+    QUADRANTE:
         
         SPACE
         IMPMATRIZ
-        SPACE
-        BARRA1
-        SPACE
+        SPACE 
+        
         IMPMATRIZ
-        SPACE
-        BARRA1
-        SPACE
-        IMPMATRIZ
-        SPACE
-        BARRA2
         SPACE
         
-        DEC DI 
+        IMPMATRIZ
+        SPACE
+        BARRA1
+        
+               
+        DEC DI  
        
         JNZ QUADRANTE
         P_LINHA
-        
+        CMP CX,4
+        JNZ OUT1 
+        DIVISAO1
+        OUT1:
+        CMP CX,7
+        JNZ OUT2
+        DIVISAO1
+        OUT2:
         ADD BX,COLUNA 
 
         LOOP VOLTA
          
         POPMACRO
         RET 
-        IMPRIME_M ENDP
+    IMPRIME_M ENDP
 
-        CENTER PROC 
+CENTER PROC 
         MOV AH,02
         MOV CX,12
         MOV DL,32
@@ -187,8 +225,14 @@ QUADRANTE:
         INT 21h
         LOOP BACK
         RET 
-        CENTER ENDP
+    CENTER ENDP
+    
+LER PROC 
+    XOR SI,SI
+    
 
+RET 
+LER ENDP
 
 
 
